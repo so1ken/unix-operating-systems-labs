10df3a452750f36a37cc3d483052f21a21f8711e
so1ken <kocienkoilya@gmail.com>
2026-09-13 13:30:40 +0700

    Добавил set -e, trap и функцию обработки ошибок

--- lab1/build.sh
+++ lab1/build.sh
@@ -1,60 +1,62 @@
 #!/bin/sh
+set -e  # Выходить при любой ошибке
+
+# Функция обработки исключений и вывода их в stderr
+exceptions(){
+    echo "Ошибка: $1" >&2
+    exit "$2"
+}
+
 file="$1"
 outputName=""
 
-if [ $# -eq 0 ]; then
-    echo "Ошибка: укажите файл"
-    exit 1
-fi
+if [ $# -eq 0 ]; then exceptions "укажите файл" 1; fi
 
-if [ ! -r "$file" ]; then
-    echo "Ошибка: файл не читается"
-    exit 2
-fi
+# Получаем абсолютный путь к файлу и папке, где он лежит
+abs_file=$(realpath "$file")
+src_dir=$(dirname "$abs_file")
 
-outputName=$(grep -m 1 "Output:" "$file" | sed 's/.*Output:[[:space:]]*//')
+if [ ! -r "$abs_file" ]; then exceptions "файл не читается" 2; fi
 
-if [ -z "$outputName" ]; then
-    echo "Ошибка: не найден комментарий"
-    exit 3
-fi
+# Добавил || true так как, в случае отсутствия Output, grep выдаст код ошибки 1 и программа немедленно завершится, до следующего if [ -z "$outputName" ]
+outputName=$(grep -m 1 "Output:" "$abs_file" | sed 's/.*Output:[[:space:]]*//' || true)
+
+if [ -z "$outputName" ]; then die "не найден комментарий Output:" 3; fi
 
 tmpDir=$(mktemp -d)
 
-clean(){
+# Исправленная обработка сигналов
+exit_handler() {
+    local rc=$?
+    trap - EXIT
     rm -rf "$tmpDir"
+    exit $rc
 }
+trap exit_handler EXIT HUP INT QUIT PIPE TERM
 
-trap clean EXIT INT TERM
+# Переходим во временный каталог
+cd "$tmpDir" || exit 1
 
-ext="${file##*.}"
+ext="${abs_file##*.}"
 
 if [ "$ext" = "c" ]; then
-    cc "$file" -o "$tmpDir/$outputName"
+    if ! cc "$abs_file" -o "$outputName"; then exceptions "компиляции C" 4; fi
 elif [ "$ext" = "cpp" ]; then
-    c++ "$file" -o "$tmpDir/$outputName"
+    if ! c++ "$abs_file" -o "$outputName"; then exceptions "компиляции C++" 4; fi
 elif [ "$ext" = "tex" ]; then
-    baseName=$(basename "$file" .tex)
+    baseName=$(basename "$abs_file" .tex)
 
-    pdflatex -output-directory "$tmpDir" "$file"
-    rc=$?
-    
-    if [ $rc -eq 0 ]; then
-        mv "$tmpDir/$baseName.pdf" "$tmpDir/$outputName.pdf"
-        outputName="$outputName.pdf"
+    if ! pdflatex -interaction=nonstopmode "$abs_file" > /dev/null; then 
+        exceptions "компиляции LaTeX" 4
     fi
+    
+    mv "$baseName.pdf" "$outputName.pdf"
+    outputName="$outputName.pdf"
 else
-    echo "Ошибка: неизвестный формат файла"
-    exit 4
-fi  
+    exceptions "неизвестный формат файла" 5
+fi 
 
-if [ $? -ne 0 ]; then
-    echo "Ошибка компиляции"
-    exit 5
-fi
-
-srcDir=$(dirname "$file")
-mv "$tmpDir/$outputName" "$srcDir/$outputName"
+mv "$outputName" "$src_dir/$outputName"
 
 echo "Сборка завершена успешно"
 exit 0
